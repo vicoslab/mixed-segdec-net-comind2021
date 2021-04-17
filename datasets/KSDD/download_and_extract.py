144fdf3e120af412ce78eb287076e982d322a5c7
Domen Tabernik <domen.tabernik@gmail.com>
2021-04-17 11:17:15 +0200

    Added missing download_and_extract.py files

---
+++ datasets/KSDD/download_and_extract.py
@@ -0,0 +1,11 @@
+from io import BytesIO
+from urllib.request import urlopen
+from zipfile import ZipFile
+
+if __name__ == "__main__":
+
+    zipurl = 'http://go.vicos.si/kolektorsddboxes'
+
+    with urlopen(zipurl) as zipresp:
+        with ZipFile(BytesIO(zipresp.read())) as zfile:
+            zfile.extractall('.')
