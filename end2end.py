65cda8e059bb28042ff23538458fd342d895b4c8
Matic <matic687@gmail.com>
2022-01-23 18:18:38 +0100

    Added datetime logging

--- end2end.py
+++ end2end.py
@@ -14,6 +14,7 @@ import random
 import cv2
 from config import Config
 from torch.utils.tensorboard import SummaryWriter
+from datetime import datetime
 
 LVL_ERROR = 10
 LVL_INFO = 5
@@ -31,7 +32,8 @@ class End2End:
         self.storage_path: str = os.path.join(self.cfg.RESULTS_PATH, self.cfg.DATASET)
 
     def _log(self, message, lvl=LVL_INFO):
-        n_msg = f"{self.run_name} {message}"
+        time = datetime.now().strftime("%d-%m-%y %H:%M:%S")
+        n_msg = f"{time} {self.run_name} {message}"
         if lvl >= LOG:
             print(n_msg)
 
